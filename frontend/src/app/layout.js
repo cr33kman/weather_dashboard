4a488e768d4266e85a9e271a9df358d9d2d472d7
becckman <benja.beckman@gmail.com>
2024-06-11 00:00:22 +0200

    feat(gui): added frontend skeleton

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <h1 className="font-bold underline text-3xl">Hello, World!</h1>
+      </body>
+    </html>
+  );
+}
